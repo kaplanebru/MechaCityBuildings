87dd0b8b61cb10acf74070d52f1397d2db1b82e3
kaplanebru <kaplanebru687@gmail.com>
2026-05-03 21:05:44 +0300

    rotation working on each side

--- Assets/Scripts/Grid/SlotCreator.cs
+++ Assets/Scripts/Grid/SlotCreator.cs
@@ -13,19 +13,15 @@ public class SlotCreator
         {
             var slotData = new SlotData(quad.data.Coords);
 
-            var missingNeighborSum = Vector2Int.zero;
-            foreach (var point in quad.data.Neighbors) //missinglere burdan da bakılabilir
+            slotData.Rotation *= Quaternion.Euler(0, -90, 0);
+            
+            if (TryGetMissingNeighborNormalSum(quad, map, slotData, out var missingNeighborNormalSum))
             {
-                if(map.Contains(point))
-                    slotData.Neighbors.Add(new NeighborCell(point));
-                else
-                    missingNeighborSum += point;
+                slotData.SetType(true);
+                slotData.Rotation = Quaternion.LookRotation(GetForwardDirection(missingNeighborNormalSum), Vector3.up);
             }
-
-            slotData.SetType(missingNeighborSum != Vector2Int.zero);
-
-            if (slotData.Type == SlotType.Boundary)
-                slotData.Rotation = Quaternion.LookRotation(GetForwardDirection(missingNeighborSum), Vector3.up);
+            else
+                slotData.SetType(false);
             
             slotData.SlotSize = quad.data.WidthHeight;
             slotData.Center = quad.Center;
@@ -33,23 +29,56 @@ public class SlotCreator
 
             slotDatas.Add(slotData);
         }
-        
+
         return slotDatas;
     }
-    
-    public static Vector3 GetForwardDirection(Vector2Int missingNeighborSum)
+
+    private static bool TryGetMissingNeighborNormalSum(QuadOnMap quad, HashSet<Vector2Int> map, SlotData slotData, out Vector2Int missingNeighborNormalSum)
+    {
+        missingNeighborNormalSum = Vector2Int.zero;
+        foreach (var point in quad.data.Neighbors) //missinglere burdan da bakılabilir
+        {
+            if (map.Contains(point))
+                slotData.Neighbors.Add(new NeighborCell(point));
+            else
+            {
+                var pointCenter = new Vector2(point.x + 0.5f, point.y + 0.5f);
+                var direction = pointCenter - quad.Center;
+                
+                var normal = new Vector2Int(Math.Sign(direction.x), Math.Sign(direction.y));
+                missingNeighborNormalSum += normal;
+
+                //Debug.Log($"point: {pointCenter}, center: {quad.Center}, direction: {direction}");
+            }
+        }
+        return missingNeighborNormalSum != Vector2Int.zero;
+    }
+
+    private static Vector3 GetForwardDirection(Vector2Int missingNeighborSum)
     {
         var outwardNormal = new Vector2Int(
             Math.Sign(missingNeighborSum.x),
             Math.Sign(missingNeighborSum.y));
         
-        Vector2Int tangent = new Vector2Int(-outwardNormal.y, outwardNormal.x); //perpendicular
-        Vector3 forward = new Vector3(tangent.x, 0f, tangent.y);
-
+        bool isCorner = outwardNormal.x != 0 && outwardNormal.y != 0;
+        if(isCorner)
+            outwardNormal = GetFacingDirectionForCorners(missingNeighborSum);
+        
+        Vector2Int tangent = new Vector2Int(outwardNormal.y, -outwardNormal.x); //perpendicular
+        var forward = new Vector3(tangent.x, 0f, tangent.y);
+        
         return forward;
     }
     
-    
+    private static Vector2Int GetFacingDirectionForCorners(Vector2Int outwardNormal)
+    {
+        if (Math.Abs(outwardNormal.x) >= Math.Abs(outwardNormal.y))
+            return new Vector2Int(Math.Sign(outwardNormal.x), 0);
+        
+        return new Vector2Int(0, Math.Sign(outwardNormal.y));
+    }
+
+
     private static void FindOrientationsForSlots(HashSet<SlotData> slotDatas)
     {
         var boundarySlots = CellRegistry.GetBoundaries(slotDatas);
@@ -61,15 +90,13 @@ public class SlotCreator
                 Debug.LogWarning("OutwardNormal is zero");
                 continue;
             }
-            
+
             Vector2Int tangent = new Vector2Int(
-                -boundarySlot.OutwardNormal.y, 
+                -boundarySlot.OutwardNormal.y,
                 boundarySlot.OutwardNormal.x); //perpendicular
-            
+
             Vector3 forward = new Vector3(tangent.x, 0f, tangent.y);
             boundarySlot.Rotation = Quaternion.LookRotation(forward, Vector3.up);
         }
     }
-
-    
 }
